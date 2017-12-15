f7a2d47a3aabb006dafbf0ecd06596c37a70bbd0
vonElfvin <elvin.granat@gmail.com>
2017-12-15 23:41:45 +0100

    Added login via email, facebook and google, as well as signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'
 import { CoreModule } from './core/core.module';
--- src/app/logins/logins.module.ts
+++ src/app/logins/logins.module.ts
@@ -1,30 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginsComponent } from './logins.component';
 // import { HeroService } from './shared/hero.service';
 import { FacebookComponent } from './facebook/facebook.component';
 import {LoginsRoutingModule} from './logins-routing.module';
 import {CoreModule} from '../core/core.module';
-import { MatTabsModule, MatCardModule } from '@angular/material';
-import { GmailComponent } from './gmail/gmail.component';
+import { MatTabsModule, MatCardModule, MatButtonModule, MatInputModule } from '@angular/material';
+import { GoogleComponent } from './google/google.component';
 import { EmailComponent } from './email/email.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     CoreModule,
     LoginsRoutingModule,
     MatTabsModule,
     MatCardModule,
+    MatButtonModule,
+    MatInputModule,
     BrowserAnimationsModule,
   ],
   declarations: [
     LoginsComponent,
     FacebookComponent,
-    GmailComponent,
+    GoogleComponent,
     EmailComponent,
+    SignupComponent,
   ],
   providers: [
   ]
