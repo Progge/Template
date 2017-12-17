925ec5529260cda64612bab2452da8d64991a4bc
vonElfvin <elvin.granat@gmail.com>
2017-12-17 14:14:21 +0100

    Refactored the navbar, sidebar and added navigation possibilities

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
 import { NavComponent } from './nav/nav.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import {
@@ -13,6 +16,8 @@ import { FirebaseModule } from './firebase/firebase.module';
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    RouterModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
