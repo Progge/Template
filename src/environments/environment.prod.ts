7c0e95b2e30ffb3acea9a0392c6931d01776560e
ProffOak <skrot1234@hotmail.com>
2017-12-21 10:56:25 +0100

    added firebase and .travis.yml to trigger on master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAlak1M86TXWOeI0Wlfv5IOD2xUcukXgsw',
+    authDomain: 'progge-ab.firebaseapp.com',
+    databaseURL: 'https://progge-ab.firebaseio.com',
+    projectId: 'progge-ab',
+    storageBucket: 'progge-ab.appspot.com',
+    messagingSenderId: '1013250598786'
+  }
 };
