32c3c05d5600de25bcd19831562bee51f8ca4dd0
vonElfvin <elvin.granat@gmail.com>
2017-12-25 14:45:38 +0100

    #24 Component for hero button added

---
+++ src/app/heroes/hero-favorite/hero-favorite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-favorite',
+  templateUrl: './hero-favorite.component.html',
+  styleUrls: ['./hero-favorite.component.css']
+})
+export class HeroFavoriteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
